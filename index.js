f234d5f24bc8cfaa70e5f9a3040c988e6778df91
Raghav Dewangan <raghav.dewangan2004@gmail.com>
2023-08-13 21:21:20 -0700

    Fully finished looping logic, have to add working simple GUI for the future

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var playerPts = 0;
+var cpuPts = 0;
+
 function getComputerChoice(){
     let val = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     if (val == 1) {
@@ -25,39 +28,51 @@ function round(playerSelection, computerSelection){
     if(playerSelection == "rock"){
         if (computerSelection == "rock")
         {
-            return "Tie"
+            return "Tie, rock ties against rock"
         }
         else if(computerSelection == "scissors"){
-            return "You Win"
+            playerPts += 1;
+            return "You Win, rock beats scissors"
+            
         }
         else{
-            return "You Lose"
+            cpuPts += 1;
+            return "You Lose, rock loses against paper"
+            
         }
     }
 
     if(playerSelection == "paper"){
         if (computerSelection == "rock")
         {
-            return "You Win"
+            playerPts += 1;
+            return "You Win, paper beats rock"
+            
         }
         else if(computerSelection == "scissors"){
-            return "You Lose"
+            cpuPts += 1;
+            return "You Lose, paper loses against scissors"
+            
         }
         else{
-            return "Tie"
+            return "Tie, paper ties against paper"
         }
     }
 
     if(playerSelection == "scissors"){
         if (computerSelection == "rock")
         {
-            return "You Lose"
+            cpuPts += 1;
+            return "You Lose, scissors loses to rock"
+            
         }
         else if(computerSelection == "scissors"){
-            return "Tie"
+            return "Tie, scissors ties against scissors"
         }
         else{
-            return "You Win"
+            playerPts += 1;
+            return "You Win, Scissors beats paper"
+            
         }
     }
 
@@ -65,8 +80,32 @@ function round(playerSelection, computerSelection){
 
 }
 
-let userInput = window.prompt("Type Rock, Paper, or Scissors for your choice:").toLowerCase();
-let compSel = getComputerChoice();
-console.log("Your input: " + userInput + " Computer Input: " + compSel);
-console.log(round(userInput, compSel));
+//let userInput = window.prompt("Type Rock, Paper, or Scissors for your choice:").toLowerCase();
+//let compSel = getComputerChoice();
+//console.log("Your input: " + userInput + " Computer Input: " + compSel);
+//console.log(round(userInput, compSel));
 
+function game(){
+    
+    while (playerPts < 5 && cpuPts < 5){
+        let userInput = window.prompt("Type Rock, Paper, or Scissors for your choice:").toLowerCase();
+        let compSel = getComputerChoice();
+        console.log("Your input: " + userInput + " Computer Input: " + compSel);
+        console.log(round(userInput, compSel));
+        console.log("Your points: " + playerPts);
+        console.log("Computer Points: " + cpuPts);
+    }
+    
+    if (playerPts == 5){
+        console.log("YOU WIN!!!")
+        playerPts = 0;
+        cpuPts = 0;
+    }
+    else{
+        console.log("YOU LOSE!!!")
+        playerPts = 0;
+        cpuPts = 0;
+    }
+}
+
+game();
