50540b7177d88368fec13bfb476a1f4f2f20d2b6
Raghav Dewangan <raghav.dewangan2004@gmail.com>
2023-08-13 18:49:04 -0700

    relatively fully working rock paper scissors, need to add loop function "game"

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 function getComputerChoice(){
     let val = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     if (val == 1) {
-        return "Rock"
+        return "rock"
     }
     else if (val == 2) {
-        return "Paper"
+        return "paper"
     }
     else{
-        return "Scissors"
+        return "scissors"
     }
 }
 
-console.log(getComputerChoice())
+//console.log(getComputerChoice())
 
-const userInput = window.prompt("Choose Rock, Paper, or Scissors:");
+
+
+//console.log(userInput)
 
 
 function round(playerSelection, computerSelection){
-    computerSelection = getComputerChoice();
-    
+    //computerSelection = getComputerChoice();
+    //playerSelection = userInput.toLowerCase() //non case sensitive input;
+
+    if(playerSelection == "rock"){
+        if (computerSelection == "rock")
+        {
+            return "Tie"
+        }
+        else if(computerSelection == "scissors"){
+            return "You Win"
+        }
+        else{
+            return "You Lose"
+        }
+    }
+
+    if(playerSelection == "paper"){
+        if (computerSelection == "rock")
+        {
+            return "You Win"
+        }
+        else if(computerSelection == "scissors"){
+            return "You Lose"
+        }
+        else{
+            return "Tie"
+        }
+    }
+
+    if(playerSelection == "scissors"){
+        if (computerSelection == "rock")
+        {
+            return "You Lose"
+        }
+        else if(computerSelection == "scissors"){
+            return "Tie"
+        }
+        else{
+            return "You Win"
+        }
+    }
+
+
+
 }
+
+let userInput = window.prompt("Type Rock, Paper, or Scissors for your choice:").toLowerCase();
+let compSel = getComputerChoice();
+console.log("Your input: " + userInput + " Computer Input: " + compSel);
+console.log(round(userInput, compSel));
+
